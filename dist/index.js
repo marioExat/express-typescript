eaea0d93f6c3f73e8ad9ebf39125aed1f5fbadb8
Your Name <you@example.com>
2023-12-14 10:04:42 +0530

    added prettier

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-app.get('/', function (req, res) {
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
     res.send('Hello from express and typescript');
 });
-var port = process.env.PORT || 3000;
-app.listen(port, function () { return console.log("App listening on PORT ".concat(port)); });
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
+//# sourceMappingURL=index.js.map
